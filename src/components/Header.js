8e9faf1894764228382984247c566336455325cf
Marjorie Ngoupende <marjorie.ngoupende@gmail.com>
2021-08-12 11:46:31 +0200

    Fix modifs css + format code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,35 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-import chainsList from "../chains/chains.json"
+import chainsList from "../chains/chains.json";
 
 export default function Header(props) {
-
-    return(
-        <header>
-          <div className='container'>
-            <img src="./assets/spendcoin.png" className="rounded float-start" alt="logo spendcoin"/>dApp SpendCoin.io
-          </div>
-          <div id="user-profile">
-              {props.network.name}
-            <button type="button" className="btn btn-light btn-lg login" style={{backgroundColor: "#8248e4"}}>
-              Polygon
-            </button>
-            <button onClick={props.handleClick} type="button" className="btn btn-warning btn-lg login">
-              Connect Wallet
-            </button>
-          </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="container">
+        <img
+          src="./assets/spendcoin.png"
+          className="rounded float-start"
+          alt="logo spendcoin"
+        />
+      </div>
+      {props.network.name}
+      <div id="user-profile">
+        <button
+          type="button"
+          className="btn btn-light btn-lg login"
+          style={{ backgroundColor: "#8248e4" }}
+        >
+          Polygon
+        </button>
+        &nbsp;
+        <button
+          onClick={props.handleClick}
+          type="button"
+          className="btn btn-warning btn-lg login"
+        >
+          Connect Wallet
+        </button>
+      </div>
+    </header>
+  );
 }
