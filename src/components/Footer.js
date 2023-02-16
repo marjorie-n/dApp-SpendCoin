12acfd5de2ad74f0aa93bfd9d596496f53164a5c
Margotte83 <marjorie.ngoupende@gmail.com>
2023-02-16 17:43:08 +0100

    style: modifications css & responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,16 @@ export default function Footer() {
     <footer>
       © 2021 SpendCoinDApp make with ❤️ by &nbsp;
       <a rel="noreferrer" target="_blank">
-        <strong>Wild Team for Digital Pilote</strong>
+        <strong>
+          Wild Team for
+          <a
+            href="https://www.linkedin.com/company/digital-copilote/"
+            rel="noreferrer"
+            target="_blank"
+          >&nbsp;
+            Digital Pilote
+          </a>
+        </strong>
       </a>
     </footer>
   );
